7b89a0ae3f246c403034fd97c224f2cc80c5b0ab
Михаил <bezrukv.misha@rambler.ru>
2014-06-11 17:50:53 +0400

    Dictionary

---
+++ Dictionary/ConsoleApplication25/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree t = new Tree();
+            int n = 50;
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                t.Insert(rnd.Next(1, 200),"a");
+            t.Insert(1,"b");
+            t.Delete(1);
+            int min = t.Minimum(t.root).key;
+            Console.WriteLine(min);
+            Console.ReadKey();
+        }
+    }
+}
